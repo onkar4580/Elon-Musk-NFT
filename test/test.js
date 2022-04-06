45bb093683a7796682e39ea56f926d20be15b90f
Onkar Kumar <63390852+onkar4580@users.noreply.github.com>
2022-04-06 16:55:29 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,1 +1,18 @@
+const { expect } = require('chai');
 
+describe("ElonNFT", function() {
+
+    it("should return correct name and symbol", async function () {
+
+        const ElonNFT = await hre.ethers.getContractFactory("ElonNFT");
+
+        const ElonNFTDeployed = await ElonNFT.deploy("ElonMusk", 
+        "ELON");
+
+        await ElonNFTDeployed.deployed(); 
+
+        expect(await ElonNFTDeployed.name()).to.equal("ElonMusk");
+        expect(await ElonNFTDeployed.symbol()).to.equal("ELON");
+    });
+
+});
